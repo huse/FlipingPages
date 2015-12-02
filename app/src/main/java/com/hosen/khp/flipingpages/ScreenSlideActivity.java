59c2b9ea1e8bc271d8d7bcd604f7f96cab73b5a8
hus.kpr@gmail.com <hus.kpr@gmail.com>
2015-12-02 10:13:47 -0800

    Some changes

--- app/src/main/java/com/hosen/khp/flipingpages/ScreenSlideActivity.java
+++ app/src/main/java/com/hosen/khp/flipingpages/ScreenSlideActivity.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2012 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.hosen.khp.flipingpages;
 
 import android.app.Fragment;
@@ -65,6 +49,9 @@ public class ScreenSlideActivity extends FragmentActivity {
         mPager = (ViewPager) findViewById(R.id.pager);
         mPagerAdapter = new ScreenSlidePagerAdapter(getFragmentManager());
         mPager.setAdapter(mPagerAdapter);
+        mPager.setPageTransformer(true, new ZoomOutPageTransformer());
+        //mPager.setPageTransformer(true, new DepthPageTransformer());
+
         mPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
             @Override
             public void onPageSelected(int position) {
